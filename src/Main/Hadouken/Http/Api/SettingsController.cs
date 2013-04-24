1a2fd621a8c8b781dfa8ba1b88c9857291432506
Viktor Elofsson <viktor@viktorelofsson.se>
2013-04-24 21:08:45 +0200

    Added the SettingsController, responsible for getting and setting settings.

---
+++ src/Main/Hadouken/Http/Api/SettingsController.cs
@@ -0,0 +1,55 @@
+﻿using Hadouken.Configuration;
+using Hadouken.Data;
+using Hadouken.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Hadouken.Http.Api
+{
+    public class SettingsController : HttpApiController
+    {
+        private readonly IDataRepository _repository;
+
+        public SettingsController(IDataRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public object Get()
+        {
+            return new
+                {
+                    settings = (from setting in _repository.List<Setting>()
+                                where setting.Key != "auth.password"
+                                select new []
+                                    {
+                                        setting.Key.ToLowerInvariant(),
+                                        GetSettingType(setting.Type),
+                                        Newtonsoft.Json.JsonConvert.DeserializeObject(setting.Value),
+                                        setting.Permissions,
+                                        setting.Options
+                                    })
+                };
+        }
+
+        private int GetSettingType(string type)
+        {
+            switch (type)
+            {
+                case "System.Int32":
+                    return 0;
+
+                case "System.Boolean":
+                    return 1;
+
+                case "System.String":
+                    return 2;
+
+                default:
+                    return -1;
+            }
+        }
+    }
+}
