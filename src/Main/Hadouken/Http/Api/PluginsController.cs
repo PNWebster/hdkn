85353807ffcf78df5af711b264b79b6d13536f31
Viktor Elofsson <viktor@viktorelofsson.se>
2013-04-24 16:04:04 +0200

    Added a PluginsController for listing plugins.

---
+++ src/Main/Hadouken/Http/Api/PluginsController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Hadouken.Plugins;
+
+namespace Hadouken.Http.Api
+{
+    public class PluginsController : HttpApiController
+    {
+        private readonly IPluginEngine _pluginEngine;
+
+        public PluginsController(IPluginEngine pluginEngine)
+        {
+            _pluginEngine = pluginEngine;
+        }
+
+        public object Get()
+        {
+            return new
+                {
+                    plugins = (from plugin in _pluginEngine.Managers
+                               select new
+                                   {
+                                       plugin.Value.Name,
+                                       plugin.Value.Version
+                                   }).ToArray()
+                };
+        } 
+    }
+}
